b012cc04ea6c5a71d7861b0eef2ad8dac40b9ee8
briecoyle <brie.coyle@gmail.com>
2017-09-02 22:16:08 -0400

    Add form fields for categories and content

--- my-react-blog-site-client/src/components/posts_new.js
+++ my-react-blog-site-client/src/components/posts_new.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label}</label>
         <input
-          type='text'
+        className="form-control"
+          type={field.type}
           {...field.input}
         />
       </div>
@@ -19,8 +21,22 @@ class PostsNew extends Component {
         <h3>New Post</h3>
         <form>
           <Field
+            label='Title'
             name='title'
-            component={this.renderTitleField}
+            type='text'
+            component={this.renderField}
+          />
+          <Field
+            label='Categories'
+            name='categories'
+            type='text'
+            component={this.renderField}
+          />
+          <Field
+            label='Content'
+            name='content'
+            type='text'
+            component={this.renderField}
           />
         </form>
       </div>
