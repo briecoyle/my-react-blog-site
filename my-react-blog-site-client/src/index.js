f84bf65acd5d583a31a5aacaee5c9231031d0d2f
briecoyle <brie.coyle@gmail.com>
2017-09-02 21:18:23 -0400

    Add action creator, action and reducer to fetch posts from api

--- my-react-blog-site-client/src/index.js
+++ my-react-blog-site-client/src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
 import './index.css';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path='/' component={PostsIndex}/>
-        
+
       </div>
     </BrowserRouter>
   </Provider>
