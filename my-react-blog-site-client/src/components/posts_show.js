1206669b80fe5e69cef2467f484d16a1066fd6d4
briecoyle <brie.coyle@gmail.com>
2017-09-04 18:14:16 -0400

    Add buttons and functional navigation to delete a post

--- my-react-blog-site-client/src/components/posts_show.js
+++ my-react-blog-site-client/src/components/posts_show.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -17,9 +26,20 @@ class PostsShow extends Component {
 
     return (
       <div>
-        <h3>{post.title}</h3>
-        <h6>{post.categories}</h6>
-        <p>{post.content}</p>
+        <div>
+          <Link to="/" className="btn btn-primary pull-xs-left">Back</Link>
+        </div>
+        <div>
+          <h3>{post.title}</h3>
+          <h6>{post.categories}</h6>
+          <p>{post.content}</p>
+        </div>
+        <button
+          className='btn btn-danger pull-xs-right'
+          onClick={this.onDeleteClick.bind(this)}
+          >
+            Delete Post
+          </button>
       </div>
     )
   }
@@ -29,4 +49,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
