4c1d6914a1404507104e1401ac069d45250f2494
briecoyle <brie.coyle@gmail.com>
2017-09-14 11:57:02 -0400

    Add styling to navbar

--- my-react-blog-site-client/src/components/posts_index.js
+++ my-react-blog-site-client/src/components/posts_index.js
@@ -22,11 +22,6 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
-        <div className="text-xs-right">
-          <Link className="btn btn-primary" to='/posts/new'>
-            Add a Post
-          </Link>
-        </div>
         <h3>Posts</h3>
         <ul className='list-group'>
           {this.renderPosts()}
--- my-react-blog-site-client/src/containers/navbar.js
+++ my-react-blog-site-client/src/containers/navbar.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>This is a navigation bar.</div>
+      <nav>
+        <div className="logo">
+          My React Blog Site
+        </div>
+        <div className="first-link">
+          <Link className="btn btn-info" to='/'>
+            Index
+          </Link>
+        </div>
+        <div className="second-link">
+          <Link className="btn btn-primary" to='/posts/new'>
+            Add a Post
+          </Link>
+        </div>
+      </nav>
     )
   }
 }
